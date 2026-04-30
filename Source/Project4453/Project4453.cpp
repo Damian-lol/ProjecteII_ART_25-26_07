681914eb12f1694190498cfa38a8a8c329681e86
yanire09 <yanire.sotomayor@estudiant.enti.cat>
2026-04-30 18:31:06 +0200

    cambios edu

---
+++ Source/Project4453/Project4453.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Project4453.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project4453, "Project4453" );
